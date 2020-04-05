c9ab857f3cbe9e0290923ad901121a351e2c3bd2
James Forsyth <forsythjames@hotmail.co.uk>
2020-04-05 20:16:13 +0100

    Commented build script

--- build.ps1
+++ build.ps1
@@ -1,12 +1,20 @@
+# Build Script
+
+# Need name, version, and factorio_version from info.json
 $info = Get-Content '.\src\info.json' | Out-String | ConvertFrom-Json
 
+# Mod must go in a folder named with this specific format
 $folder = "$($info.name)_$($info.version)"
 echo $folder
 
+# Need an intermediate folder to arrange files before zipping to the output
 $intermediate = ".\build\$folder"
 $output = ".\build\$($info.factorio_version)\$folder.zip"
 
+# Copy src to intermediate to be zipped to output, then clear up after
 Copy-Item '.\src' -Destination $intermediate -Recurse
 Compress-Archive -Path $intermediate -Force -DestinationPath $output
 Remove-Item -Path $intermediate -Recurse
+
+# Install output zip to mods folder
 Copy-Item $output -Destination "$($env:APPDATA)\Factorio\mods"
